1179a99f44e05f2a706c2a6adb1c82a92069c4ed
Ceirion Wheatman <cei_w@hotmail.co.uk>
2025-01-05 22:31:54 +0000

    added create invoice page

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -7,7 +7,6 @@ import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
-  // NOTE: Uncomment this code in Chapter 11
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
@@ -19,9 +18,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   };
   return (
     <>
-      {/*  NOTE: Uncomment this code in Chapter 11 */}
-
-      {/* <div className="inline-flex">
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -54,7 +51,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 }
